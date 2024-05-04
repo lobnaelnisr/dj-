93b3dded32fa5cba6891f6923f6fde8625bf2c63
lobnaelnisr <lobnaelnisr@gmail.com>
2024-05-04 06:22:56 +0300

    Initial commit

--- api/admin.py
+++ api/admin.py
@@ -12,6 +12,4 @@ from .models import SessionData
 class SessionDataAdmin(admin.ModelAdmin):
     list_display = ['userName','SessionStartedAt', 'arousal', 'attention', 'dominantEmotion', 'gender', 'valence', 'volume','SessionEndedAt']
     search_fields = ['userName']
-    def SessionEndedAt(self, obj):
-        # Implement logic to generate the value for the custom field
-        return "Custom Value"
+    
