906086b6a63bfea6878020972b1e55eb121bc345
Marmar9999 <119695675+Marmar9999@users.noreply.github.com>
2024-05-12 15:31:34 +0300

    smtp

--- project2/settings.py
+++ project2/settings.py
@@ -96,7 +96,8 @@ WSGI_APPLICATION = 'project2.wsgi.application'
 #        'NAME':'db.sqlite3',
 #    }
 #}
-#django.db.backends
+#django.db.backends.mysql
+#mysql.connector.django
 
 DATABASES = {
     'default': {
@@ -170,3 +171,12 @@ MEDIA_URL = "/media/"          #.
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+#SMTP Conf: (YOU ORG REAL EMAIL TO BE USED HERE AUTO)
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT ='587'                                     #for TLS
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER ='ahmedeid2504@gmail.com'             #ur email
+EMAIL_HOST_PASSWORD ='tgah icfr ocgi bkfc'         #ur password 
