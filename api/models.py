2f3275cad022426163b475ec1b9a913200f60384
Marmar9999 <119695675+Marmar9999@users.noreply.github.com>
2024-05-12 15:06:52 +0300

    session_for

--- api/models.py
+++ api/models.py
@@ -6,6 +6,7 @@ class SessionData(models.Model):
     CaptureTime = models.CharField(max_length=12, null=True, blank=True)
     SessionEndedAt = models.CharField(max_length=12, null=True, blank=True)
     SessionStartedAt = models.CharField(max_length=12, null=True, blank=True)
+    Session_for = models.CharField(max_length=255, default='')
     arousal = models.IntegerField()
     attention = models.IntegerField()
     dominantEmotion = models.CharField(max_length=50)
