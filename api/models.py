1cacc6ff3ab09a951dee779c11d74006f88a76d5
Marmar9999 <119695675+Marmar9999@users.noreply.github.com>
2024-05-13 04:30:53 +0300

    features removed

--- api/models.py
+++ api/models.py
@@ -10,15 +10,15 @@ class SessionData(models.Model):
     arousal = models.IntegerField()
     attention = models.IntegerField()
     dominantEmotion = models.CharField(max_length=50)
-    feature_1 = models.CharField(max_length=50)
-    feature_2 = models.CharField(max_length=50)
-    feature_3 = models.CharField(max_length=50)
-    feature_4 = models.CharField(max_length=50)
-    feature_5 = models.CharField(max_length=50)
-    gender = models.CharField(max_length=10)
+    #gender = models.CharField(max_length=10)
     userEmail = models.CharField(max_length=100)
     valence = models.FloatField()
     volume = models.FloatField()
+    #feature_1 = models.CharField(max_length=50)
+    #feature_2 = models.CharField(max_length=50)
+    #feature_3 = models.CharField(max_length=50)
+    #feature_4 = models.CharField(max_length=50)
+    #feature_5 = models.CharField(max_length=50)
     
     def __str__(self):
         return self.userEmail
