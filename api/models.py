f13de3cc26322b341289fdecc175f49392b55a88
lobnaelnisr <lobnaelnisr@gmail.com>
2024-05-19 07:50:42 +0300

    allowww null

--- api/models.py
+++ api/models.py
@@ -6,11 +6,11 @@ class SessionData(models.Model):
     SessionEndedAt = models.CharField(max_length=12, null=True, blank=True)
     SessionStartedAt = models.CharField(max_length=12, null=True, blank=True)
     Session_for = models.CharField(max_length=255, default='')
-    arousal = models.IntegerField()
-    attention = models.IntegerField()
+    arousal = models.IntegerField(null=True, blank=True)
+    attention = models.IntegerField(null=True, blank=True)
     dominantEmotion = models.CharField(max_length=50, null=True, blank=True)
     userEmail = models.CharField(max_length=100)
-    valence = models.FloatField()
+    valence = models.FloatField(null=True, blank=True)
     volume = models.FloatField(null=True, blank=True)
     
     def __str__(self):
