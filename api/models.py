2bed520d11e8ee7523f85451abc3785bad1bea4a
Marmar9999 <119695675+Marmar9999@users.noreply.github.com>
2024-05-21 21:59:29 +0300

    model

--- api/models.py
+++ api/models.py
@@ -2,23 +2,19 @@ from django.db import models
 
 
 class SessionData(models.Model):
-    #CaptureTime = models.TimeField(auto_now= True, blank=True)
     CaptureTime = models.CharField(max_length=12, null=True, blank=True)
     SessionEndedAt = models.CharField(max_length=12, null=True, blank=True)
     SessionStartedAt = models.CharField(max_length=12, null=True, blank=True)
     Session_for = models.CharField(max_length=255, default='')
-    arousal = models.FloatField()
-    attention = models.FloatField()
-    dominantEmotion = models.CharField(max_length=50)
-    #gender = models.CharField(max_length=10)
-    userEmail = models.CharField(max_length=100)
-    valence = models.FloatField()
-    volume = models.FloatField()
-    #feature_1 = models.CharField(max_length=50)
-    #feature_2 = models.CharField(max_length=50)
-    #feature_3 = models.CharField(max_length=50)
-    #feature_4 = models.CharField(max_length=50)
-    #feature_5 = models.CharField(max_length=50)
+    arousal = models.FloatField(null=True, blank=True)
+    attention = models.FloatField(null=True, blank=True)
+    dominantEmotion = models.CharField(max_length=50, null=True, blank=True, default='Neutral')
+    userEmail = models.EmailField(max_length=254)
+    valence = models.FloatField(null=True, blank=True)
+    volume = models.FloatField(null=True, blank=True)
+    
+    def _str_(self):
+        return self.userEmail
     
     def __str__(self):
         return self.userEmail
